8af0dfa01d03fcf40ea84b8f6c6020729744ba80
SeitaroTaketani <taketanirunning@gmail.com>
2026-05-05 14:35:03 +0200

    再修正

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/SHC-tradeMap-UNCTADstyled/',
+  base: '/SHC-trade-visualization/',
   root: '.',
   publicDir: 'public',
   server: {
